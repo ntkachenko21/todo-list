f673383f72a818a60c67c1947342d1b17a8cfe7b
Nikita Tkachenko <7heswatgg@gmail.com>
2025-07-11 15:20:15 +0200

    improve queries and add cache to views

--- task/forms.py
+++ task/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from django.forms.models import ModelForm
-
 from task.models import Task, Tag
-
 
 class TaskUpdateForm(ModelForm):
     class Meta:
@@ -13,15 +11,67 @@ class TaskUpdateForm(ModelForm):
             "content",
             "deadline",
             "is_done",
+            "tags",
         )
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Describe what needs to be done...'
+            }),
+            'deadline': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'type': 'datetime-local'
+            }),
+            'is_done': forms.CheckboxInput(attrs={
+                'class': 'form-checkbox'
+            }),
+        }
 
-class TaskForm(ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
         required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'tags-checkbox-list'
+        })
     )
 
+class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = (
+            "content",
+            "deadline",
+            "tags",
+        )
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Describe what needs to be done...'
+            }),
+            'deadline': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'type': 'datetime-local'
+            }),
+        }
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'tags-checkbox-list'
+        })
+    )
+
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = ("name",)
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter tag name (e.g., Work, Personal, Urgent)...',
+                'maxlength': 63
+            })
+        }
